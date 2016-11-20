7d99ceeb0507a3640ef9148d8252aa66bb49a7db
Unknown <denisevbaalen@gmail.com>
2016-11-20 17:44:44 +0100

    Added second compareTo function to Track class
    Impossible to pass parameters to compareTo function, sadly, so we are forced to create two compareTo functions.

--- src/nl/ru/ai/exercise1/Track.java
+++ src/nl/ru/ai/exercise1/Track.java
@@ -11,12 +11,12 @@ public class Track implements Comparable<Track>
   public Length time;    // track length
   public String country; // artist country
 /**
- * Compare this track with an other
+ * Compare this track with an other based on artist name
  * @return -1 if this track is smaller, 0 if equal and 1 if this track is larger
  */
-  public int compareTo(Track other)
+ 
+  /*public int compareTo(Track other)
   {
-    // for now, tracks are simple ordered by artist name
 	  if (artist.compareTo(other.artist)==0)
 	  {
 		  if (cd.compareTo(other.cd)==0)
@@ -37,5 +37,34 @@ public class Track implements Comparable<Track>
 		  return cd.compareTo(other.cd);
 	  }
 	  return artist.compareTo(other.artist);
+  }
+  */
+  
+  /**
+   * Compare this track with an other based on track length
+   * @return -1 if this track is smaller, 0 if equal and 1 if this track is larger
+   */
+  public int compareTo(Track other)
+  {
+	  if (time.compareTo(other.time)==0)
+	  {
+		  if (artist.compareTo(other.artist)==0)
+		  {
+			  if (cd.compareTo(other.cd)==0)
+			  {
+					  if(track<other.track)
+					  {
+						  return -1;
+					  } 
+					  else 
+					  {
+						  return 1;
+					  }
+			  }
+			  return cd.compareTo(other.cd);
+		  }
+		  return artist.compareTo(other.artist);
+	  }
+  	return time.compareTo(other.time);
   }
 }
